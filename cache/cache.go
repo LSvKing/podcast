054d8d6c8f4371c44df534f78e203796d0b482bb
lsvking <lsvking@gmail.com>
2017-08-20 01:18:42 +0800

    使用ds

--- cache/cache.go
+++ cache/cache.go
@@ -4,24 +4,20 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
-
-	"github.com/adelowo/onecache"
-	"github.com/adelowo/onecache/filesystem"
 
 	"cloud.google.com/go/datastore"
 )
 
-func New() onecache.Store {
-	store := filesystem.MustNewFSStore("data/cache", 10*time.Hour)
-	//store, err := onecache.Get("fs")
-	//
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-
-	return store
-}
+//func New() onecache.Store {
+//	store := filesystem.MustNewFSStore("data/cache", 10*time.Hour)
+//	//store, err := onecache.Get("fs")
+//	//
+//	//if err != nil {
+//	//	log.Panic(err)
+//	//}
+//
+//	return store
+//}
 
 type Cache struct {
 	Key  string
