799f44d70ef113dc209078f49b7dbf930d475e1b
lsvking <lsvking@gmail.com>
2017-10-03 20:57:31 +0800

    使用onecache

--- cache/cache.go
+++ cache/cache.go
@@ -1,85 +1,35 @@
 package cache
 
 import (
-	"context"
 	"fmt"
 
-	"cloud.google.com/go/datastore"
+	"time"
+
+	"github.com/adelowo/onecache"
+	"github.com/adelowo/onecache/filesystem"
 )
 
-//func New() onecache.Store {
-//	store := filesystem.MustNewFSStore("data/cache", 10*time.Hour)
-//	//store, err := onecache.Get("fs")
-//	//
-//	//if err != nil {
-//	//	log.Panic(err)
-//	//}
-//
-//	return store
-//}
+var store onecache.Store
 
-type Cache struct {
-	Key  string
-	Body []byte `datastore:",noindex"`
+func init() {
+	store = filesystem.MustNewFSStore("data/cache", time.Hour*24*30)
 }
 
 func Get(key string) ([]byte, error) {
-	ctx := context.Background()
 
-	// Set your Google Cloud Platform project ID.
-	projectID := "lsvproxyv1"
-
-	// Creates a client.
-	client, err := datastore.NewClient(ctx, projectID)
-
-	if err != nil {
-		fmt.Println("Failed to create client:", err)
-	}
-
-	// Sets the kind for the new entity.
-	kind := "Podcast"
-	// Sets the name/ID for the new entity.
-	//name := key
-
-	var c Cache
-
-	k := datastore.NameKey(kind, key, nil)
-
-	if err := client.Get(ctx, k, &c); err != nil {
+	if body, err := store.Get(key); err != nil {
 		return []byte(""), err
 	} else {
-		return c.Body, err
+		return body, err
 	}
 }
 
 func Set(key string, body []byte) {
-	ctx := context.Background()
 
-	// Set your Google Cloud Platform project ID.
-	projectID := "lsvproxyv1"
-
-	// Creates a client.
-	client, err := datastore.NewClient(ctx, projectID)
+	err := store.Set(key, body, time.Hour*24*30)
 
 	if err != nil {
-		fmt.Println("Failed to create client:", err)
+		fmt.Println(err)
 	}
-
-	kind := "Podcast"
-
-	// Creates a Key instance.
-	k := datastore.NameKey(kind, key, nil)
-
-	c := Cache{
-		Key:  key,
-		Body: body,
-	}
-
-	// k
-	if _, err := client.Put(ctx, k, &c); err != nil {
-		fmt.Printf("Failed to save task: %v", err)
-	}
-
-	fmt.Printf("Saved %v: %v\n", k, c.Key)
 
 }
